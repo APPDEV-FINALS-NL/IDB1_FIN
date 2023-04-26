0eff3badc47db385d36081442e4cdb9e8bcb9f91
Ericka Nasingao <20217209@s.ubaguio.edu>
2023-04-26 12:54:08 +0800

    4/26/23

--- BabiFy/src/app/categories/categories.component.ts
+++ BabiFy/src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Bib } from 'src/bibs';
+import { BIBS } from 'src/bibs-lists';
 
 @Component({
   selector: 'app-categories',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent {
-
+  bibs = BIBS;
+  title = 'Bibs Lists';
+  selectedBib?: Bib;
+  
+  onSelect(bib: Bib): void{
+    this.selectedBib = bib;
+  }
 }
