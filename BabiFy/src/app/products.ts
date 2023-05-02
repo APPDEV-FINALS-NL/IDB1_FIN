95f1b663cfc3a7ba7aafe92bf5e42a63e8faca23
APPDEV-FINALS-NL <131742136+APPDEV-FINALS-NL@users.noreply.github.com>
2023-05-02 22:14:36 +0800

    Create products.ts

---
+++ BabiFy/src/app/products.ts
@@ -0,0 +1,90 @@
+[
+    {
+    id: 1,
+    pname: 'Silicone Baby Bibs Easily Wipe Clean',
+    price: '₱199',
+    color: 'Turquoise/Lime Green',
+    material: 'Silicone',
+    brand: 'Happy Healthy Parent',
+    type: 'Button',
+    pattern: 'Solid',
+    imageUrl:'assets/bibs/bib1.jpg'
+},
+{
+    id: 2,
+    pname: 'Tiny Twinkle Roundabout Drool Bibs',
+    price: '₱175',
+    color: 'Cinnamon',
+    material: 'Cotton',
+    brand: 'Tiny Twinkle',
+    type: 'Snap',
+    pattern: 'Solid',
+    imageUrl: 'assets/bibs/bib2.jpg'
+},
+{
+    id: 3,
+    pname: 'Konssy Muslin Baby Bandana Drool Bibs',
+    price: '₱550',
+    color: 'Assorted',
+    material: 'Cotton',
+    brand: 'Konssy',
+    type: 'Snap',
+    pattern: 'Solid',
+    imageUrl:'assets/bibs/bib3.jpg'
+},
+{
+    id: 4,
+    pname: 'The Peanutshell Terry Bib Set for Baby Boys',
+    price: '₱359',
+    color: 'Multicolor',
+    material: 'Polyester, Cotton',
+    brand: 'The Peanutshell',
+    type: 'Hook and Loop',
+    pattern: 'Letter Print',
+    imageUrl:'assets/bibs/bib4.jpg'
+},
+{
+    id: 5,
+    pname: 'Bumkins Unisex Baby Feeding Bib',
+    price: '₱259',
+    color: 'Hangry',
+    material: 'Cotton',
+    brand: 'Bumkins',
+    type: 'Hook and Loop',
+    pattern: 'Cartoon',
+    imageUrl:'assets/bibs/bib5.jpg'
+},
+{
+    id: 6,
+    pname: 'Tiny Twinkle Mess-Proof Baby Bib',
+    price: '₱300',
+    color: 'Boho Unicorn',
+    material: 'Polyester',
+    brand: 'Tiny Twinkle',
+    type: 'Hook and Loop',
+    pattern: 'Animal Print',
+    imageUrl:'assets/bibs/bib6.jpg'
+},
+{
+    id: 7,
+    pname: 'Muslin Baby Teething Feeding Drooling Bibs',
+    price: '₱450',
+    color: 'Pink',
+    material: 'Cotton',
+    brand: 'Nishine',
+    type: 'Snap',
+    pattern: 'Solid',
+    imageUrl:'assets/bibs/bib7.jpg'
+},
+{
+    id: 8,
+    pname: 'Diaper Squad Baby Drool Bandana Bibs',
+    price: '₱700',
+    color: 'Assorted',
+    material: 'Cotton',
+    brand: 'Diaper Squad',
+    type: 'Snap',
+    pattern: 'Solid',
+    imageUrl:'assets/bibs/bib8.jpg'
+},
+]
