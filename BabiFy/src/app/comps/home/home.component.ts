f3dfa6a1191246503bcf7b8389d9cc5ac4ff59e2
DominicChristianLaroya <98566084+DominicChristianLaroya@users.noreply.github.com>
2023-05-16 17:10:42 +0800

    5/16/23

--- BabiFy/src/app/comps/home/home.component.ts
+++ BabiFy/src/app/comps/home/home.component.ts
@@ -1,43 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+
+
 
 @Component({
   selector: '.app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-    this.startCountdown();
-  }
-@Input() init:any = null;
-public counter:number=0;
-
-constructor(){}
-startCountdown(){
-  if(this.init && this.init >0){
-    this.counter = this.init;
-    this.doCountdown();
-  }
-}
-doCountdown(){
-  setTimeout(()=>{
-    this.counter = this.counter -1;
-    this.processCountdown();
-  }, 1000)
-}
-
-processCountdown(){
-console.log("Count is ", this.counter);
-
-if (this.counter == 0){
-  console.log("End");
-
-}
-else{
-  this.doCountdown();
-}
-}
-
+export class HomeComponent  {
+  
  
-
 }
+
